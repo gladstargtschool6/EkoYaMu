876c6b2f146236d1ecf49061bb71078fe71f3c54
Joseph Moshood <gladstargtschool6@gmail.com>
2020-10-19 14:03:41 +0100

    Update ExampleInstrumentedTest.java

--- after/NoteKeeper/app/src/androidTest/java/com/jwhh/jim/notekeeper/ExampleInstrumentedTest.java
+++ after/NoteKeeper/app/src/androidTest/java/com/jwhh/jim/notekeeper/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.jwhh.jim.notekeeper", appContext.getPackageName());
+        assertEquals("com.gladstar.ekoyamu", appContext.getPackageName());
     }
 }
